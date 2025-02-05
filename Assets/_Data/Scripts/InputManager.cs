04d62d3cffae080be6c0cdd82f8f59cd236c6ca8
huukhanh2603 <147917702+huukhanh2603@users.noreply.github.com>
2025-02-05 21:14:41 +0700

    edit Của Khánh

--- Assets/_Data/Scripts/InputManager.cs
+++ Assets/_Data/Scripts/InputManager.cs
@@ -8,17 +8,16 @@ public class InputManager : LinkMonoBehaviour
     public static InputManager Instance { get => _instance; }
 
     [Header("InputManager")]
-
     protected KeyCode _lastKeyPressed;
 
     [SerializeField] protected float _moveAccelInput;
     public float MoveAccelInput { get => _moveAccelInput; }
     [SerializeField] protected float _sensitivity = 2f;
 
-	[SerializeField] protected float _moveInput;
-	public float MoveInput { get => _moveInput; }
+    [SerializeField] protected float _moveInput;
+    public float MoveInput { get => _moveInput; }
 
-	[SerializeField] protected bool _starJumpInput;
+    [SerializeField] protected bool _starJumpInput;
     public bool StarJumpInput { get => _starJumpInput; }
 
     [SerializeField] protected bool _endJumpInput;
@@ -26,78 +25,67 @@ public class InputManager : LinkMonoBehaviour
 
     [SerializeField] protected bool _attackInput;
     public bool AttackInput { get => _attackInput; }
-    
+
     [SerializeField] protected bool _dashInput;
     public bool DashInput { get => _dashInput; }
 
     protected override void Awake()
     {
         base.Awake();
-        if (InputManager._instance != null) Debug.LogError("Only 1 InputManager allow to exist");
-        InputManager._instance = this;
+        if (_instance != null)
+        {
+            Debug.LogError("Only one InputManager is allowed to exist!");
+            Destroy(gameObject);
+            return;
+        }
+        _instance = this;
     }
 
     void Update()
     {
-        this.CheckMoveInput();
-        this.CheckJumpInput();
-        this.CheckAttackInput();
-        this.CheckDashInput();
+        CheckMoveInput();
+        CheckJumpInput();
+        CheckAttackInput();
+        CheckDashInput();
     }
 
     protected virtual void CheckMoveInput()
     {
-        //Kiểm tra nhấn nút A, D khi cả 2 nút cùng được nhấn
+        bool rightPressed = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+        bool leftPressed = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (rightPressed && !leftPressed)
         {
-			_moveAccelInput = Mathf.Lerp(_moveAccelInput, 0f, Time.fixedDeltaTime * 3 / _sensitivity);
-            _moveInput = 0;
+            _moveAccelInput = Mathf.Clamp(_moveAccelInput + Time.deltaTime / _sensitivity, -1f, 1f);
+            _moveInput = 1;
             _lastKeyPressed = KeyCode.RightArrow;
         }
-
-        if (Input.GetKeyDown(KeyCode.D))
+        else if (leftPressed && !rightPressed)
         {
-			_moveAccelInput = Mathf.Lerp(_moveAccelInput, 0f, Time.fixedDeltaTime * 3 / _sensitivity);
-            _moveInput = 0;
-            _lastKeyPressed = KeyCode.D;
-        }
-
-        if (Input.GetKeyUp(KeyCode.RightArrow) && Input.GetKey(KeyCode.D)) _lastKeyPressed = KeyCode.D;
-
-        if (Input.GetKeyUp(KeyCode.D) && Input.GetKey(KeyCode.RightArrow)) _lastKeyPressed = KeyCode.RightArrow;
-
-        if (_lastKeyPressed == KeyCode.RightArrow && Input.GetKey(KeyCode.RightArrow))
-        {
-            if (_moveAccelInput > -1) _moveAccelInput -= Time.fixedDeltaTime / _sensitivity;
+            _moveAccelInput = Mathf.Clamp(_moveAccelInput - Time.deltaTime / _sensitivity, -1f, 1f);
             _moveInput = -1;
-            return;
+            _lastKeyPressed = KeyCode.LeftArrow;
         }
-
-        if (_lastKeyPressed == KeyCode.D && Input.GetKey(KeyCode.D))
+        else
         {
-            if (_moveAccelInput < 1) _moveAccelInput += Time.fixedDeltaTime / _sensitivity;
-            _moveInput = 1;
-            return;
+            _moveAccelInput = Mathf.Lerp(_moveAccelInput, 0f, Time.deltaTime * 3 / _sensitivity);
+            _moveInput = 0;
         }
-
-		_moveAccelInput = Mathf.Lerp(_moveAccelInput, 0f, Time.fixedDeltaTime * 3 / _sensitivity);
-        _moveInput = 0;
     }
 
     protected virtual void CheckJumpInput()
     {
-        this._starJumpInput = Input.GetKeyDown(KeyCode.K);
-        this._endJumpInput = Input.GetKeyUp(KeyCode.K);
+        _starJumpInput = Input.GetKeyDown(KeyCode.K);
+        _endJumpInput = Input.GetKeyUp(KeyCode.K);
     }
 
     protected virtual void CheckAttackInput()
     {
-        this._attackInput = Input.GetKey(KeyCode.J);
+        _attackInput = Input.GetKey(KeyCode.J);
     }
 
     protected virtual void CheckDashInput()
     {
-        this._dashInput = Input.GetKeyDown(KeyCode.L);
+        _dashInput = Input.GetKeyDown(KeyCode.L);
     }
 }
