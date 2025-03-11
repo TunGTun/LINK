c41477d3133340ccc8cdfbc0b79b687c18a1394f
huukhanh2603 <147917702+huukhanh2603@users.noreply.github.com>
2025-03-11 22:53:45 +0700

    11/03/2025

--- Assets/_Data/Khanh/Trap/ScorpianController.cs
+++ Assets/_Data/Khanh/Trap/ScorpianController.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScorpianAnimation : MonoBehaviour
+public class ScorpianController : MonoBehaviour
 {
-    public float speed;
-
     // Start is called before the first frame update
     void Start()
     {
