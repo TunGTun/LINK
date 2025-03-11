ab2791cf15e9e48b5249798a93f5e00d3eabe5b8
Bui-toan <145842406+Bui-toan@users.noreply.github.com>
2025-03-11 23:39:17 +0700

    level4
    11/03/2025

---
+++ Assets/ExplosionTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ExplosionTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public GameObject explosionPrefab; // Gán Prefab vụ nổ ở Inspector
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) // Nếu nhân vật chạm vào
+        {
+            GameObject explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(explosion, 0.5f); // Xóa vụ nổ sau 0.5 giây
+            Destroy(gameObject); // Xóa đối tượng va chạm
+        }
+    }
+}
