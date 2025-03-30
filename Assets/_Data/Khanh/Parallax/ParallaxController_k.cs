8c1ac59f442b99bd62d8a900c4ad8773cb84cfdc
huukhanh2603 <147917702+huukhanh2603@users.noreply.github.com>
2025-03-31 00:26:39 +0700

    31/03
    khanhbachay

---
+++ Assets/_Data/Khanh/Parallax/ParallaxController_k.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+
+public class ParallaxController_k : MonoBehaviour
+{
+    Transform cam;
+    Vector3 camStartPos;
+    float distance;
+
+    GameObject[] backgrounds;
+    Material[] mat;
+    float[] backSpeed;
+    float farthestBack;
+
+    [Range(0.01f, 0.05f)]
+    public float parallaxSpeed;
+
+    public Texture[] newTextures; // Danh sách Texture mới cho từng layer
+
+    private Vector3 lastCamPosition; // Lưu vị trí Camera trước đó
+
+    void Start()
+    {
+        cam = Camera.main.transform;
+        camStartPos = cam.position;
+        lastCamPosition = cam.position; // Lưu lại vị trí ban đầu của Camera
+
+        int backCount = transform.childCount;
+        mat = new Material[backCount];
+        backSpeed = new float[backCount];
+        backgrounds = new GameObject[backCount];
+
+        for (int i = 0; i < backCount; i++)
+        {
+            backgrounds[i] = transform.GetChild(i).gameObject;
+            mat[i] = backgrounds[i].GetComponent<Renderer>().material;
+        }
+
+        BackSpeedCalculate(backCount);
+    }
+
+    void BackSpeedCalculate(int backCount)
+    {
+        for (int i = 0; i < backCount; i++)
+        {
+            float depth = backgrounds[i].transform.position.z - cam.position.z;
+            if (depth > farthestBack)
+            {
+                farthestBack = depth;
+            }
+        }
+
+        for (int i = 0; i < backCount; i++)
+        {
+            backSpeed[i] = 1 - (backgrounds[i].transform.position.z - cam.position.z) / farthestBack;
+        }
+    }
+
+    //private void LateUpdate()
+    //{
+    //    if (cam.position == lastCamPosition) return; // Nếu Camera không di chuyển thì không làm gì cả
+
+    //    distance = cam.position.x - camStartPos.x;
+    //    transform.position = new Vector3(cam.position.x, transform.position.y, transform.position.z);
+
+    //    for (int i = 0; i < backgrounds.Length; i++)
+    //    {
+    //        float speed = backSpeed[i] * parallaxSpeed;
+    //        mat[i].SetTextureOffset("_MainTex", new Vector2(distance * speed, 0));
+    //    }
+
+    //    lastCamPosition = cam.position; // Lưu vị trí Camera hiện tại
+    //}
+
+    private void LateUpdate()
+    {
+        if (cam.position == lastCamPosition) return;
+
+        distance = Mathf.Lerp(distance, cam.position.x - camStartPos.x, Time.deltaTime * 5f); // Làm mượt
+
+        transform.position = new Vector3(cam.position.x, transform.position.y, transform.position.z);
+
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            float speed = backSpeed[i] * parallaxSpeed;
+            mat[i].SetTextureOffset("_MainTex", new Vector2(distance * speed, 0));
+        }
+
+        lastCamPosition = cam.position;
+    }
+
+
+    // Hàm này sẽ được gọi khi nhân vật đi vào vùng thay đổi nền
+    public void ChangeParallaxTexture()
+    {
+        for (int i = 0; i < mat.Length; i++)
+        {
+            if (newTextures.Length >= i && newTextures[i] != null)
+            {
+                Debug.Log($"Đổi nền {i} thành {newTextures[i].name}");
+                mat[i].mainTexture = newTextures[i]; // Thay đổi texture
+            }
+        }
+    }
+}
