4f315dcbb13e94a0d72754de1568db402328e0aa
TunGTun <tung141004@gmail.com>
2025-05-08 21:35:00 +0700

    Level_1

---
+++ Assets/_Data/Tung/Animation/Animals/Mushroom/Mushroom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mushroom : MonoBehaviour
+{
+    public float forceMagnitude = 10f;
+    public Animator animator;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other.transform.position.y > transform.position.y)
+            {
+                Rigidbody2D playerRb = other.GetComponent<Rigidbody2D>();
+                if (playerRb != null)
+                {
+                    playerRb.velocity = Vector2.zero;
+                    animator.SetTrigger("isBounce");
+                    playerRb.AddForce(Vector2.up * forceMagnitude, ForceMode2D.Impulse);
+                }
+            }
+        }
+    }
+}
